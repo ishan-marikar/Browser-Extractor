fc81dcb5f8511a8eb773fff9b8c33d5e804a31a6
ishan-marikar <ishan.marikar@outlook.com>
2015-03-07 13:10:29 +0530

    Initial commit to add project Files
    Only added saved password exraction for chrome so far.

---
+++ BrowserPasswordRetriever/Credentials.cs
@@ -0,0 +1,10 @@
+﻿namespace BrowserPasswordRetriever
+{
+	public class Credentials
+	{
+		public string Browser { get; set; }
+		public string Site { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ BrowserPasswordRetriever/LoginReader.cs
@@ -0,0 +1,81 @@
+﻿namespace BrowserPasswordRetriever
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Security.Cryptography;
+	using System.Text;
+	using Mono.Data.Sqlite;
+
+	public class LoginReader
+	{
+		#region Public Methods and Operators
+
+		public static List<Credentials> GetChromePasswords()
+		{
+			var credentials = new List<Credentials>();
+			SqliteConnection connection = CreateConnection(GetChromePasswordsFile());
+			try
+			{
+				connection.Open();
+				const string Sql = "SELECT * FROM logins";
+				using (var command = new SqliteCommand(Sql, connection))
+				{
+					using (SqliteDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							string password = ChromeDecrypt(reader["password_value"] as byte[]);
+							credentials.Add(
+								new Credentials
+									{
+										Site = reader["signon_realm"] as string,
+										Username = reader["username_value"] as string,
+										Password = password,
+										Browser = "Google Chrome"
+									});
+						}
+					}
+				}
+				
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			finally
+			{
+				connection.Close();
+			}
+
+			return credentials;
+		}
+
+		#endregion
+
+		#region Methods
+
+		private static SqliteConnection CreateConnection(string fileName)
+		{
+			string connectionString = string.Format("Data Source={0};", fileName);
+			var connection = new SqliteConnection(connectionString);
+			return connection;
+		}
+
+		private static string ChromeDecrypt(byte[] blob)
+		{
+			byte[] decryptedBytes = ProtectedData.Unprotect(blob, null, DataProtectionScope.CurrentUser);
+			return Encoding.UTF8.GetString(decryptedBytes);
+		}
+
+		private static string GetChromePasswordsFile()
+		{
+			string localApplicationPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+			string fileName = Path.Combine(localApplicationPath, "Google", "Chrome", "User Data", "Default", "Login Data");
+
+			return fileName;
+		}
+
+		#endregion
+	}
+}
