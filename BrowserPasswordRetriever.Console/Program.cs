ec62d99a726bdaf2926947954fe2271f20d605fb
ishan-marikar <ishan.marikar@outlook.com>
2015-03-10 08:19:36 +0530

    Added header display text for console

--- BrowserPasswordRetriever.Console/Program.cs
+++ BrowserPasswordRetriever.Console/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BrowserPasswordRetriever.Console
+namespace BrowserPasswordRetriever
 {
 	class Program
 	{
 		static void Main( string[] args )
 		{
+			Console.WriteLine("Browser Password Extractor v 0.0.1");
+			Console.WriteLine("Written by Ishan Marikar (ishan.marikar@outlook.com)");
 		}
 	}
 }
